eb4770ea53d76da65e117bcbcf22b83a0bff0e1d
jchanvfx <jjohnnycchan@gmail.com>
2019-05-30 19:52:24 +1200

    version bump

--- NodeGraphQt/__init__.py
+++ NodeGraphQt/__init__.py
@@ -30,7 +30,7 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __status__ = 'Work in Progress'
 __license__ = 'MIT'
 
