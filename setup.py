2e6b76056a5b5f518a6e47ecaffab211ffd40381
Johnny Chan <7546915+jchanvfx@users.noreply.github.com>
2020-01-23 11:45:28 +1300

    typo fix in classifiers #148

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ classifiers = [
     'Operating System :: OS Independent',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.7',
-    'Documentation' :: 'https://jchanvfx.github.io/NodeGraphQt/api/html/index.html',
-    'Source' :: 'https://github.com/jchanvfx/NodeGraphQt/',
+    'Documentation :: https://jchanvfx.github.io/NodeGraphQt/api/html/index.html',
+    'Source :: https://github.com/jchanvfx/NodeGraphQt/',
 ]
 
 setuptools.setup(
