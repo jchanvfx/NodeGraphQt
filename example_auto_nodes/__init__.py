686930d698cf247e15745497c6a78f28adbd54ab
jchanvfx <jjohnnycchan@gmail.com>
2021-04-26 22:12:16 +1200

    rm modules

--- example_auto_nodes/__init__.py
+++
@@ -1,3 +0,0 @@
-from .node_base import AutoNode, ModuleNode, SubGraphNode, RootNode
-from .subgraph_nodes import Publish
-from .node_base.utils import update_node_down_stream, update_nodes, setup_node_menu
