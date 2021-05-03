ba6edeb6c3f9dc4f6f9255b637861b1533935f99
mara004 <65915611+mara004@users.noreply.github.com>
2021-05-03 15:44:10 +0200

    Fix typo
    pardon

--- NodeGraphQt/widgets/viewer.py
+++ NodeGraphQt/widgets/viewer.py
@@ -5,7 +5,7 @@ import math
 import Qt
 from Qt import QtGui, QtCore, QtWidgets
 
-# use QOpenGLWidget instead of the deprecated QGLWdiegt to avoid probelms with Wayland
+# use QOpenGLWidget instead of the deprecated QGLWidget to avoid probelms with Wayland
 if Qt.IsPySide2:
     from PySide2.QtWidgets import QOpenGLWidget
 elif Qt.IsPyQt5:
