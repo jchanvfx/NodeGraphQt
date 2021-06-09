ed4077884b68f64b9fbb5b981ef87c9ac1a3e204
jchan <jjohnnycchan@gmail.com>
2021-06-09 17:04:35 +1200

    updated viewer nav

--- NodeGraphQt/widgets/viewer_nav.py
+++ NodeGraphQt/widgets/viewer_nav.py
@@ -47,21 +47,22 @@ class NodeNavigationDelagate(QtWidgets.QStyledItemDelegate):
             h = rect.height()
             x, y = rect.left() + 2.0, rect.top()
             painter.setBrush(itm_color)
-            for i in range(3):
+            for count in range(2):
                 x += 2.8
                 y += m
                 h -= m * 2
                 itm_rect = QtCore.QRectF(x, y, 1.3, h)
                 painter.drawRoundedRect(itm_rect, 1.0, 1.0)
         else:
+            x = rect.left() + 2.0
             size = 10.0
             for clr in [QtGui.QColor(0, 0, 0, 80), itm_color]:
-                itm_rect = QtCore.QRectF(rect.center().x() - (size / 2),
-                                         rect.center().y() - (size / 2),
-                                         size, size)
+                itm_rect = QtCore.QRectF(
+                    x, rect.center().y() - (size / 2), size, size)
                 painter.setBrush(clr)
                 painter.drawRoundedRect(itm_rect, 2.0, 2.0)
                 size -= 5.0
+                x += 2.5
 
         # text
         pen_color = option.palette.text().color()
@@ -76,7 +77,7 @@ class NodeNavigationDelagate(QtWidgets.QStyledItemDelegate):
         )
         font_height = font_metrics.height()
         text_rect = QtCore.QRectF(
-            rect.center().x() - (font_width / 2) + 5.0,
+            rect.center().x() - (font_width / 2) + 4.0,
             rect.center().y() - (font_height / 2),
             font_width, font_height)
         painter.drawText(text_rect, item.text())
@@ -85,13 +86,15 @@ class NodeNavigationDelagate(QtWidgets.QStyledItemDelegate):
 
 class NodeNavigationWidget(QtWidgets.QListView):
 
+    node_item_selected = QtCore.Signal(str, list)
+
     def __init__(self, parent=None):
         super(NodeNavigationWidget, self).__init__(parent)
         self.setSelectionMode(self.SingleSelection)
-        self.setDragEnabled(False)
         self.setResizeMode(self.Adjust)
         self.setViewMode(self.ListMode)
         self.setFlow(self.LeftToRight)
+        self.setDragEnabled(False)
         self.setMinimumHeight(20)
         self.setMaximumHeight(26)
         self.setSpacing(0)
@@ -99,17 +102,35 @@ class NodeNavigationWidget(QtWidgets.QListView):
         self.setItemDelegate(NodeNavigationDelagate(self))
         self.setModel(QtGui.QStandardItemModel())
 
+        self.clicked.connect(self._on_item_clicked)
+
+    def keyPressEvent(self, event):
+        event.ignore()
+
+    def _on_item_clicked(self, index):
+        rows = reversed(range(1, self.model().rowCount()))
+        if index.row() == 0:
+            rows = [r for r in rows if r > 0]
+        else:
+            rows = [r for r in rows if index.row() <= r]
+        rm_node_ids = [self.model().item(r, 0).toolTip() for r in rows]
+        node_id = self.model().item(index.row(), 0).toolTip()
+        [self.model().removeRow(r) for r in rows]
+        self.node_item_selected.emit(node_id, rm_node_ids)
+
     def clear(self):
         self.model().sourceMode().clear()
 
-    def add_item(self, label):
-        width = 20
+    def add_label_item(self, label, node_id):
         item = QtGui.QStandardItem(label)
-        if label:
-            metrics = QtGui.QFontMetrics(item.font())
-            width = metrics.horizontalAdvance(item.text()) + 28
+        item.setToolTip(node_id)
+        metrics = QtGui.QFontMetrics(item.font())
+        width = metrics.horizontalAdvance(item.text()) + 26
         item.setSizeHint(QtCore.QSize(width, 20))
         self.model().appendRow(item)
+        self.selectionModel().setCurrentIndex(
+            self.model().indexFromItem(item),
+            QtCore.QItemSelectionModel.ClearAndSelect)
 
 
 if __name__ == '__main__':
@@ -118,6 +139,10 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     widget = NodeNavigationWidget()
+    widget.add_label_item('Root', 'root')
+    for i in range(1, 4):
+        widget.add_label_item('group node {}'.format(i),
+                              'node_id{}'.format(i))
     widget.show()
 
     app.exec_()
