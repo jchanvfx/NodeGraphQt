4bd262234087b0eb14784255a5b9db4d9decbc5a
jchan <jjohnnycchan@gmail.com>
2022-05-18 22:37:35 +1200

    minor fix to setup #255

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from setuptools import setup
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import setuptools
+
+from NodeGraphQt import pkg_info
 
 if __name__ == '__main__':
-    setup()
+    setuptools.setup(
+        name=pkg_info.__module_name__,
+        version=pkg_info.__version__,
+        author=pkg_info.__author__,
+    )
