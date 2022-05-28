44ab6e02c8e6210dbb029536cf6c63cb0b48e8c9
jchan <jjohnnycchan@gmail.com>
2022-05-28 23:52:41 +1200

    clean up

--- NodeGraphQt/constants.py
+++ NodeGraphQt/constants.py
@@ -12,6 +12,43 @@ __doc__ = """
  throughout the whole NodeGraphQt library.
 """
 
+# ================================== PRIVATE ===================================
+
+URI_SCHEME = 'nodegraphqt://'
+URN_SCHEME = 'nodegraphqt::'
+
+# === PATHS ===
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+ICON_PATH = os.path.join(BASE_PATH, 'widgets', 'icons')
+ICON_DOWN_ARROW = os.path.join(ICON_PATH, 'down_arrow.png')
+ICON_NODE_BASE = os.path.join(ICON_PATH, 'node_base.png')
+
+# === DRAW STACK ORDER ===
+
+Z_VAL_PIPE = -1
+Z_VAL_NODE = 1
+Z_VAL_PORT = 2
+Z_VAL_NODE_WIDGET = 3
+
+# === ITEM CACHE MODE ===
+
+# QGraphicsItem.NoCache
+# QGraphicsItem.DeviceCoordinateCache
+# QGraphicsItem.ItemCoordinateCache
+
+ITEM_CACHE_MODE = QtWidgets.QGraphicsItem.DeviceCoordinateCache
+
+# === NODE LAYOUT DIRECTION ===
+
+#: Mode for vertical node layout.
+NODE_LAYOUT_VERTICAL = 0
+#: Mode for horizontal node layout.
+NODE_LAYOUT_HORIZONTAL = 1
+#: Variable for setting the node layout direction.
+# NODE_LAYOUT_DIRECTION = NODE_LAYOUT_VERTICAL
+NODE_LAYOUT_DIRECTION = NODE_LAYOUT_HORIZONTAL
+
 # =================================== GLOBAL ===================================
 
 
@@ -28,6 +65,93 @@ class VERSION(Enum):
     MINOR = int(_v.split('.')[1])
     #:
     PATCH = int(_v.split('.')[2])
+
+# =================================== VIEWER ===================================
+
+
+class VIEWER_STYLING(Enum):
+    """
+    Node graph viewer styling layout:
+    :py:mod:`NodeGraphQt.constants.VIEWER_STYLING`
+    """
+    #: default background color for the node graph.
+    BACKGROUND_COLOR = (35, 35, 35)
+    #: style node graph background with no grid or dots.
+    GRID_DISPLAY_NONE = 0
+    #: style node graph background with dots.
+    GRID_DISPLAY_DOTS = 1
+    #: style node graph background with grid lines.
+    GRID_DISPLAY_LINES = 2
+    #: grid size when styled with grid lines.
+    GRID_SIZE = 50
+    #: grid line color.
+    GRID_COLOR = (45, 45, 45)
+
+
+class VIEWER_NAV_STYLING(Enum):
+    """
+    Node graph viewer navigation styling layout:
+    :py:mod:`NodeGraphQt.constants.VIEWER_NAV_STYLING`
+    """
+    #: default background color.
+    BACKGROUND_COLOR = (25, 25, 25)
+    #: default item color.
+    ITEM_COLOR = (35, 35, 35)
+
+# ==================================== NODE ====================================
+
+
+class NODE_STYLING(Enum):
+    """
+    Node styling layout:
+    :py:mod:`NodeGraphQt.constants.NODE_STYLING`
+    """
+    #: default node width.
+    WIDTH = 160
+    #: default node height.
+    HEIGHT = 60
+    #: default node icon size (WxH).
+    ICON_SIZE = 18
+    #: default node overlay color when selected.
+    SELECTED_COLOR = (255, 255, 255, 30)
+    #: default node border color when selected.
+    SELECTED_BORDER_COLOR = (254, 207, 42, 255)
+
+# ==================================== PORT ====================================
+
+
+class PORT_STYLING(Enum):
+    """
+    Port styling layout:
+    :py:mod:`NodeGraphQt.constants.PORT_STYLING`
+    """
+    #: default port size.
+    SIZE = 22.0
+    #: default port color. (r, g, b, a)
+    COLOR = (49, 115, 100, 255)
+    #: default port border color.
+    BORDER_COLOR = (29, 202, 151, 255)
+    #: port color when selected.
+    ACTIVE_COLOR = (14, 45, 59, 255)
+    #: port border color when selected.
+    ACTIVE_BORDER_COLOR = (107, 166, 193, 255)
+    #: port color on mouse over.
+    HOVER_COLOR = (17, 43, 82, 255)
+    #: port border color on mouse over.
+    HOVER_BORDER_COLOR = (136, 255, 35, 255)
+    #: threshold for selecting a port.
+    CLICK_FALLOFF = 15.0
+
+
+class PORT_TYPE(Enum):
+    """
+    Port connection types:
+    :py:mod:`NodeGraphQt.constants.PORT_TYPE`
+    """
+    #: Connection type for input ports.
+    IN = 'in'
+    #: Connection type for output ports.
+    OUT = 'out'
 
 # ==================================== PIPE ====================================
 
@@ -78,63 +202,8 @@ class PIPE_LAYOUT(Enum):
     #: draw angled lines for pipe connections.
     ANGLE = 2
 
-# ==================================== PORT ====================================
 
-
-class PORT_STYLING(Enum):
-    """
-    Port styling layout:
-    :py:mod:`NodeGraphQt.constants.PORT_STYLING`
-    """
-    #: default port size.
-    SIZE = 22.0
-    #: default port color. (r, g, b, a)
-    COLOR = (49, 115, 100, 255)
-    #: default port border color.
-    BORDER_COLOR = (29, 202, 151, 255)
-    #: port color when selected.
-    ACTIVE_COLOR = (14, 45, 59, 255)
-    #: port border color when selected.
-    ACTIVE_BORDER_COLOR = (107, 166, 193, 255)
-    #: port color on mouse over.
-    HOVER_COLOR = (17, 43, 82, 255)
-    #: port border color on mouse over.
-    HOVER_BORDER_COLOR = (136, 255, 35, 255)
-    #: threshold for selecting a port.
-    CLICK_FALLOFF = 15.0
-
-
-class PORT_TYPE(Enum):
-    """
-    Port connection types:
-    :py:mod:`NodeGraphQt.constants.PORT_TYPE`
-    """
-    #: Connection type for input ports.
-    IN = 'in'
-    #: Connection type for output ports.
-    OUT = 'out'
-
-
-# ==================================== NODE ====================================
-
-
-class NODE_STYLING(Enum):
-    """
-    Node styling layout:
-    :py:mod:`NodeGraphQt.constants.NODE_STYLING`
-    """
-    #: default node width.
-    WIDTH = 160
-    #: default node height.
-    HEIGHT = 60
-    #: default node icon size (WxH).
-    ICON_SIZE = 18
-    #: default node overlay color when selected.
-    SELECTED_COLOR = (255, 255, 255, 30)
-    #: default node border color when selected.
-    SELECTED_BORDER_COLOR = (254, 207, 42, 255)
-
-# === NODE PROPERTY ===
+# === PROPERTY BIN WIDGET ===
 
 #: Property type will hidden in the properties bin (default).
 NODE_PROP = 0
@@ -170,73 +239,3 @@ NODE_PROP_FLOAT = 15
 NODE_PROP_INT = 16
 #: Property type represented with button widget in the properties bin.
 NODE_PROP_BUTTON = 17
-
-# === NODE VIEWER ===
-
-
-class VIEWER_STYLING(Enum):
-    """
-    Node graph viewer styling layout:
-    :py:mod:`NodeGraphQt.constants.VIEWER_STYLING`
-    """
-    #: default background color for the node graph.
-    BACKGROUND_COLOR = (35, 35, 35)
-    #: style node graph background with no grid or dots.
-    GRID_DISPLAY_NONE = 0
-    #: style node graph background with dots.
-    GRID_DISPLAY_DOTS = 1
-    #: style node graph background with grid lines.
-    GRID_DISPLAY_LINES = 2
-    #: grid size when styled with grid lines.
-    GRID_SIZE = 50
-    #: grid line color.
-    GRID_COLOR = (45, 45, 45)
-
-
-class VIEWER_NAV_STYLING(Enum):
-    """
-    Node graph viewer navigation styling layout:
-    :py:mod:`NodeGraphQt.constants.VIEWER_NAV_STYLING`
-    """
-    #: default background color.
-    BACKGROUND_COLOR = (25, 25, 25)
-    #: default item color.
-    ITEM_COLOR = (35, 35, 35)
-
-
-# ================================== PRIVATE ===================================
-
-URI_SCHEME = 'nodegraphqt://'
-URN_SCHEME = 'nodegraphqt::'
-
-# === PATHS ===
-
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-ICON_PATH = os.path.join(BASE_PATH, 'widgets', 'icons')
-ICON_DOWN_ARROW = os.path.join(ICON_PATH, 'down_arrow.png')
-ICON_NODE_BASE = os.path.join(ICON_PATH, 'node_base.png')
-
-# === DRAW STACK ORDER ===
-
-Z_VAL_PIPE = -1
-Z_VAL_NODE = 1
-Z_VAL_PORT = 2
-Z_VAL_NODE_WIDGET = 3
-
-# === ITEM CACHE MODE ===
-
-# QGraphicsItem.NoCache
-# QGraphicsItem.DeviceCoordinateCache
-# QGraphicsItem.ItemCoordinateCache
-
-ITEM_CACHE_MODE = QtWidgets.QGraphicsItem.DeviceCoordinateCache
-
-# === NODE LAYOUT DIRECTION ===
-
-#: Mode for vertical node layout.
-NODE_LAYOUT_VERTICAL = 0
-#: Mode for horizontal node layout.
-NODE_LAYOUT_HORIZONTAL = 1
-#: Variable for setting the node layout direction.
-# NODE_LAYOUT_DIRECTION = NODE_LAYOUT_VERTICAL
-NODE_LAYOUT_DIRECTION = NODE_LAYOUT_HORIZONTAL
