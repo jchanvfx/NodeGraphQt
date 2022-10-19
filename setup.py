459009f1f0fcb08ec86f290a5519c58c83f2940c
jchan <jjohnnycchan@gmail.com>
2022-10-19 23:56:49 +1300

    setup clean up #269

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-import setuptools
-
-from NodeGraphQt import pkg_info
+from setuptools import setup
 
 if __name__ == '__main__':
-    setuptools.setup(
-        name=pkg_info.__module_name__,
-        version=pkg_info.__version__,
-        author=pkg_info.__author__,
-    )
+    setup()
