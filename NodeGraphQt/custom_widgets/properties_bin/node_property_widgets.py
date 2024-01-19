8ea5876a0a33a9eadc99feaa070b82d126bc13ce
jchan <jjohnnycchan@gmail.com>
2024-01-19 16:32:04 +1300

    exposed NodePropEditorWidget widget object.

--- NodeGraphQt/custom_widgets/properties_bin/node_property_widgets.py
+++ NodeGraphQt/custom_widgets/properties_bin/node_property_widgets.py
@@ -302,13 +302,13 @@ class _PortConnectionsContainer(QtWidgets.QWidget):
             chb_widget.setDisabled(disable)
 
 
-class NodePropWidget(QtWidgets.QWidget):
+class NodePropEditorWidget(QtWidgets.QWidget):
     """
-    Node properties widget for display a Node object.
+    Node properties editor widget for display a Node object.
 
     Args:
         parent (QtWidgets.QWidget): parent object.
-        node (NodeGraphQt.BaseNode): node.
+        node (NodeGraphQt.NodeObject): node.
     """
 
     #: signal (node_id, prop_name, prop_value)
@@ -316,7 +316,7 @@ class NodePropWidget(QtWidgets.QWidget):
     property_closed = QtCore.Signal(str)
 
     def __init__(self, parent=None, node=None):
-        super(NodePropWidget, self).__init__(parent)
+        super(NodePropEditorWidget, self).__init__(parent)
         self.__node_id = node.id
         self.__tab_windows = {}
         self.__tab = QtWidgets.QTabWidget()
@@ -537,6 +537,15 @@ class NodePropWidget(QtWidgets.QWidget):
         self.__tab.addTab(self.__tab_windows[name], name)
         return self.__tab_windows[name]
 
+    def get_tab_widget(self):
+        """
+        Returns the underlying tab widget.
+
+        Returns:
+            QtWidgets.QTabWidget: tab widget.
+        """
+        return self.__tab
+
     def get_widget(self, name):
         """
         get property widget.
@@ -741,6 +750,20 @@ class PropertiesBinWidget(QtWidgets.QWidget):
         if not self._block_signal:
             self.property_changed.emit(node_id, prop_name, prop_value)
 
+    def create_property_editor(self, node):
+        """
+        Creates a new property editor widget from the provided node.
+
+        (re-implement for displaying custom node property editor widget.)
+
+        Args:
+            node (NodeGraphQt.NodeObject): node object.
+
+        Returns:
+            NodePropEditorWidget: property editor widget.
+        """
+        return NodePropEditorWidget(node=node)
+
     def limit(self):
         """
         Returns the limit for how many nodes can be loaded into the bin.
@@ -779,7 +802,7 @@ class PropertiesBinWidget(QtWidgets.QWidget):
 
         self._prop_list.insertRow(0)
 
-        prop_widget = NodePropWidget(node=node)
+        prop_widget = self.create_property_editor(node=node)
         prop_widget.property_closed.connect(self.__on_prop_close)
         prop_widget.property_changed.connect(self.__on_property_widget_changed)
         port_connections = prop_widget.get_port_connection_widget()
