033c5f6f95e030747890ad9b5a7d9767f93cb4e2
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-12-27 17:06:41 +0100

    feat (NodeWidgets) : add NodeSpinBox as default Widget

--- NodeGraphQt/widgets/node_widgets.py
+++ NodeGraphQt/widgets/node_widgets.py
@@ -383,6 +383,75 @@ class NodeLineEdit(NodeBaseWidget):
         if text != self.get_value():
             self.get_custom_widget().setText(text)
             self.on_value_changed()
+class NodeSpinBox(NodeBaseWidget):
+    """
+    Displays as a ``QSpinBox`` in a node.
+
+    .. inheritance-diagram:: NodeGraphQt.widgets.node_widgets.NodeSpinBox
+        :parts: 1
+
+    .. note::
+        `To embed a` ``QSpinBox`` `in a node see func:`
+        :meth:`NodeGraphQt.BaseNode.add_text_input`
+    """
+
+    def __init__(self, parent=None, name='', label='', value=0, min_value=0, max_value=100):
+        super(NodeSpinBox, self).__init__(parent, name, label)
+        bg_color = ViewerEnum.BACKGROUND_COLOR.value
+        text_color = tuple(map(lambda i, j: i - j, (255, 255, 255),
+                               bg_color))
+        text_sel_color = text_color
+        style_dict = {
+            'QSpinBox': {
+                'background': 'rgba({0},{1},{2},20)'.format(*bg_color),
+                'border': '1px solid rgb({0},{1},{2})'
+                          .format(*ViewerEnum.GRID_COLOR.value),
+                'border-radius': '3px',
+                'color': 'rgba({0},{1},{2},150)'.format(*text_color),
+                'selection-background-color': 'rgba({0},{1},{2},100)'
+                                              .format(*text_sel_color),
+            }
+        }
+        stylesheet = ''
+        for css_class, css in style_dict.items():
+            style = '{} {{\n'.format(css_class)
+            for elm_name, elm_val in css.items():
+                style += '  {}:{};\n'.format(elm_name, elm_val)
+            style += '}\n'
+            stylesheet += style
+        spin_box = QtWidgets.QSpinBox()
+        spin_box.setRange(min_value, max_value)
+        spin_box.setValue(value)
+        spin_box.setStyleSheet(stylesheet)
+        spin_box.setAlignment(QtCore.Qt.AlignCenter)
+        spin_box.editingFinished.connect(self.on_value_changed)
+        spin_box.clearFocus()
+        self.set_custom_widget(spin_box)
+        self.widget().setMaximumWidth(140)
+
+    @property
+    def type_(self):
+        return 'SpinBoxNodeWidget'
+
+    def get_value(self):
+        """
+        Returns the widgets current value.
+
+        Returns:
+            str: current text.
+        """
+        return str(self.get_custom_widget().value())
+
+    def set_value(self, value=0):
+        """
+        Sets the widgets current text.
+
+        Args:
+            text (int): new value.
+        """
+        if value != self.get_value():
+            self.get_custom_widget().setValue(value)
+            self.on_value_changed()
 class NodeButton(NodeBaseWidget):
     """
     Displays as a ``QPushButton`` in a node.
